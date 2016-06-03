c3162eacbb04bc0f208ea1096b9137a9712c612e
olavxxx <olavamjelde@gmail.com>
2016-06-03 23:02:38 +0200

    Web interface
    C3JS web interface, bare minimum.

---
+++ vevgrensesnitt/README.md
@@ -0,0 +1,2 @@
+CSV-Data forked from elzapp @ github
+Minimal c3js charts template by olavxxx @ github
---
+++ vevgrensesnitt/script.js
@@ -0,0 +1,45 @@
+// Code goes here
+
+$(document).ready(function() {
+  var prepend = "https://raw.githubusercontent.com/olavxxx/sykkelstats/master/svv-tellepunkt/";
+  var myStringArray = ["kanalveien", "fjosangerveien", "floenstien", "puddefjordsbroen", "sandviken", "solheimsviken","haavardstun","skjoldskiftet"];
+  var append = ".csv";
+
+  var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      x: 'måned', //the x-axis
+      xFormat: '%Y-%m',
+      url: prepend + myStringArray[0] + append,
+      mimeType: 'csv'
+    },
+    axis: {
+
+      x: {
+        type: 'timeseries',
+        localtime: false,
+        tick: {
+          format: '%Y-%m'
+        }
+      }
+    }
+
+  });
+
+
+  var arrayLength = myStringArray.length;
+  for (var i = 1; i < arrayLength; i++) {
+    loadMore(prepend + myStringArray[i] + append);
+  }
+
+  function loadMore(theurl) {
+      chart.load({
+        url: theurl,
+        mimeType: 'csv',
+        type: 'line'
+
+      });
+  }
+
+
+});
